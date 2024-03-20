16a68f7d776a0038237c5a204e9f9d4e0851741b
Andrea <andhy.lopez.090@gmail.com>
2024-03-20 00:00:17 +0000

    nuevos cambios

--- Ejercicios/Ejercicios.js
+++ Ejercicios/Ejercicios.js
@@ -7,3 +7,9 @@ console.log(nombre);
 // Crea una variable llamada `temperaturaCelsius` y asígnale un valor en 
 // grados Celsius. Convierte esta temperatura a grados Fahrenheit utilizando 
 // la fórmula `F = (C *  9/5) + 32` y muestra el resultado en la consola.
+
+let temperaturaCelsius = 25;
+
+let temperaturaFahrenheit = (temperaturaCelsius * 9 / 5) + 32;
+
+console.log(temperaturaFahrenheit)
