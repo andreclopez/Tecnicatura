ed999df687bf40924e2ca1bdac37f664edb890c6
Andrea <andhy.lopez.090@gmail.com>
2024-03-27 11:12:59 +0000

    nuevos cambios

--- Examen/examen.js
+++ Examen/examen.js
@@ -3,50 +3,73 @@
 // dependencia de: Nota es menor a 4 - LIBRE, Nota es menor a 7 y no es 
 // LIBRE - REGULAR Nota mayor que 6  - PROMOCIONADO.
 
-console.log("Ejercicio simulacro")
+//console.log("Ejercicio simulacro")
 
-function condicionAlumno(nota){
+//function condicionAlumno(nota){
 
-    if(nota < 4) {
-        return "LIBRE"
-    } else if (nota < 7) {
-        return "REGULAR"
-    } else if (nota > 6) {
-        return "PROMOCIONADO"
-}
-}
+ //   if(nota < 4) {
+ //       return "LIBRE"
+  //  } else if (nota < 7) {
+ //       return "REGULAR"
+ //   } else if (nota > 6) {
+  //      return "PROMOCIONADO"
+//}
+//}
 
-let nota = parseInt(prompt("Ingrese nota"))
+//let nota = parseInt(prompt("Ingrese nota"))
 
-console.log(condicionAlumno(nota))
+//console.log(condicionAlumno(nota))
 
 // Sin funcion 
 
-console.log("Alternativa sin función")
+//console.log("Alternativa sin función")
 
-let calificacion = nota;
+//let calificacion = nota;
 
-if(nota > 1 && nota < 4){
+//if(nota > 1 && nota < 4){
 
-    console.log("LIBRE")
+ //   console.log("LIBRE")
+//}
+
+//if(nota > 4 && nota < 7){
+
+  //  console.log("REGULAR")
+//}
+
+//if(nota <= 10 && nota > 6){
+
+//    console.log("PROMOCIONADO")
+//}
+
+//if(nota > 10){
+
+//    console.log("Ingrese una nota válida")
+//}
+
+//if(nota < 1){
+
+//    console.log("Ingrese uan nota válida")
+//}
+
+// Unión de función más la alternativa que propongo
+
+console.log("Resolución propuesta final")
+
+function condicionAlumno(nota){
+
+    if(nota >= 1 && nota < 4){
+        return "LIBRE"
+    } else if(nota > 4 && nota < 7){
+        return "REGULAR"
+    } else if(nota <= 10 && nota > 6){
+        return "PROMOCIONADO"
+    } else if(nota > 10){
+        return "Ingrese una nota válida"
+    } else if(nota < 1){
+        return "Ingrese una nota válida"
+    }
 }
 
-if(nota > 4 && nota < 7){
+let nota = parseInt(prompt("Ingrese una nota"))
 
-    console.log("REGULAR")
-}
-
-if(nota <= 10 && nota > 6){
-
-    console.log("PROMOCIONADO")
-}
-
-if(nota > 10){
-
-    console.log("Ingrese una nota válida")
-}
-
-if(nota < 1){
-
-    console.log("Ingrese uan nota válida")
-}
+console.log(condicionAlumno(nota))
