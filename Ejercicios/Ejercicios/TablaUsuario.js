1a9205f46ccead3b4ee82d45df4de9174785c59e
andreclopez <andhy.lopez.090@gmail.com>
2024-09-17 01:00:24 +0100

    Nuevos cambios

--- Ejercicios/Ejercicios/TablaUsuario.js
+++ Ejercicios/Ejercicios/TablaUsuario.js
@@ -1,18 +1,53 @@
-let boton = document.getElementById("boton");
-let tabla = document.getElementById("tablaUsuario")
-
-boton.addEventListener("click", () =>{
-    let nombre = document.getElementById("nombre");
-    let email = document.getElementById("email");
-    let edad = document.getElementById("edad");
-    tabla.innerHTML += `<tr>
-    <td>${nombre.value}</td>
-    <td>${email.value}</td>
-    <td>${edad.value}</td>
-    </tr>`
-    nombre.value=""
-    email.value=""
-    edad.value=""
-
-})
+function updateTablaUsuarios() {
+    const tablaUsuarios = document.getElementById("tablaUsuarios");
+    const localStorage = window.localStorage;
     
+    tablaUsuarios.innerHTML = "";
+
+    let usuarios = JSON.parse(localStorage.getItem("users"));
+    
+    if (usuarios === undefined || usuarios === null) {
+        usuarios = [];
+    }
+
+    usuarios.forEach(user => {
+        tablaUsuarios.innerHTML += `
+            <tr>
+                <td>${user.nombre}</td>
+                <td>${user.email}</td>
+                <td>${user.edad}</td>
+            </tr>
+        `;
+    });
+}
+
+updateTablaUsuarios();
+
+document.getElementById("guardar").addEventListener("click", () => {
+    let nombre = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let edad = document.getElementById("edad").value;
+
+    let user = {
+        nombre: nombre, 
+        email: email, 
+        edad: edad
+    };
+
+    let usuarios = JSON.parse(localStorage.getItem("users"));
+
+    if (usuarios === null) {
+        usuarios = [user];
+    } else {
+        usuarios.push(user);
+    }
+
+    localStorage.setItem("users", JSON.stringify(usuarios));
+
+    updateTablaUsuarios();
+
+    // Limpiar campos
+    document.getElementById("nombre").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("edad").value = "";
+});
