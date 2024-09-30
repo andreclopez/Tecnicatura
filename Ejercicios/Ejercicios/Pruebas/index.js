78d2e1e1f10ae4f432577239b5c13ee5484d6d95
andreclopez <andhy.lopez.090@gmail.com>
2024-09-30 22:19:50 +0100

    Nuevos cambios

---
+++ Ejercicios/Ejercicios/Pruebas/index.js
@@ -0,0 +1,5 @@
+let numero = 0;
+while (numero < 10) {
+  numero = Math.floor(Math.random() * 20);
+  console.log(numero);
+}
--- Ejercicios/ProgramacionI/Clases/index.js
+++ Ejercicios/ProgramacionI/Clases/index.js
@@ -3,9 +3,20 @@ import Usuario from "./class/Usuario.js";
 // Crear un usuario 
 let ingresoUsuario = Usuario.crear('Andrea', 39, 'andhy.lopez.090@gmail.com');
 
-// Mostrar info consola
-console.log(`Nombre: ${ingresoUsuario.nombre}, Edad: ${ingresoUsuario.edad}, Email: ${ingresoUsuario.email}`);
+document.getElementById("cambioPin").addEventListener("submit", function(event) {
+    let pinActual = document.getElementById('pinActual').value;
+    let nuevoPin = document.getElementById('nuevoPin').value;
+    let nuevoPin1 = document.getElementById('nuevoPin1').value;
 
-// Cambio de PIN
-let cambioExitoso = ingresoUsuario.cambiarPin('2558', '2568');
-console.log(cambioExitoso?'El PIN se ha cambiao exitosamente.' : 'El PIN actual es incorrecto')
+    if (nuevoPin !== nuevoPin1) {
+        alert('Ambos PINS deben ser iguales. Por favor, verifique la información.')
+    }
+
+    let cambioSolicitado = ingresoUsuario.cambiarPin(pinActual, nuevoPin);
+    if (cambioSolicitado) {
+        alert('El PIN ha sido modificado de forma exitosa.');
+    } else {
+        alert('El PIN actual es incorrecto. Por favor, verifique la información.');
+    }
+
+});
