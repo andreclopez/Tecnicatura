89ee0251ad5faf9d546ab68c525b600a6079a5d6
andreclopez <andhy.lopez.090@gmail.com>
2024-10-10 16:31:53 +0100

    Nuevos Cambios

---
+++ Examen/Programcion_2revision/index.js
@@ -0,0 +1,39 @@
+import Articulo from "./classes/I_Articulo.js";
+
+function updateTablaArticulo() {
+    let tablaArticulo = document.querySelector(".tablaArticulo");
+    tablaArticulo.innerHTML = "";
+
+    Articulo.items.forEach(articulo => {
+        tablaArticulo.innerHTML += `
+            <tr id="${articulo.id}">
+                <td>${articulo.id}</td>
+                <td>${articulo.titulo}</td>
+                <td>${articulo.año}</td>
+                <td>
+                    <button data-id="${articulo.id}" class="eliminar">Eliminar</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+updateTablaArticulo();
+
+document.getElementById("agregar").addEventListener("click", () => {
+    let id = document.getElementById("id").value;
+    let titulo = document.getElementById("titulo").value;
+    let año = document.getElementById("año").value;
+
+    Articulo.crear(id, titulo, año);
+    document.getElementById("formArticulo").reset();
+    updateTablaArticulo();
+});
+
+document.querySelector(".tablaArticulo").addEventListener("click", (event) => {
+    if (event.target.className === "eliminar") {
+        let idArticulo = event.target.dataset.id;
+        Articulo.eliminar(idArticulo);
+        updateTablaArticulo();
+    }
+});
